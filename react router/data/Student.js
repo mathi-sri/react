a0dab1d760268a6579ec1d3341ab53d7dfde6f53
mathi-sri <warshnishreemathi@gmail.com>
2026-09-08 12:00:02 +0530

    student details

---
+++ react router/data/Student.js
@@ -0,0 +1,28 @@
+const students = [
+  {
+    id: 1,
+    name: "Arun",
+    age: 20,
+    course: "Computer Science",
+  },
+  {
+    id: 2,
+    name: "Kumar",
+    age: 21,
+    course: "Information Technology",
+  },
+  {
+    id: 3,
+    name: "Priya",
+    age: 20,
+    course: "Software Engineering",
+  },
+  {
+    id: 4,
+    name: "Ravi",
+    age: 22,
+    course: "Data Science",
+  },
+];
+
+export default students;
